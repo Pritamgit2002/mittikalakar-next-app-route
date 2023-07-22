e93eb7da807906e1a76168aad37f1c9b4bd31af8
swarnadeep <aritrasarkar2002@gmail.com>
2023-07-22 16:54:27 +0530

    nav updates

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -45,7 +45,15 @@ const Navbar = () => {
           <div className="hidden md:flex items-center gap-[50px]">
             {navLinks.map((links) => (
               <Link key={links.id} href={links.url}>
-                {links.title}
+                <div className="group overflow-hidden relative cursor-pointer text-normal uppercase  leading-6">
+                  <span className="inline-block p-1 transition duration-500 ease-out group-hover:-translate-y-[120%]">
+                    {links.title}
+                  </span>
+
+                  <span className="absolute left-0 translate-y-[180%] rotate-45  p-1 transition duration-500 ease-out group-hover:translate-y-0 group-hover:rotate-0">
+                    {links.title}
+                  </span>
+                </div>
               </Link>
             ))}
           </div>
